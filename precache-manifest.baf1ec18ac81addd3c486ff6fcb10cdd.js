c5c4251774cff5155900334ba68a8cb0fe0adac6
SunkariSanghavi <sunkarisanghavi@gmail.com>
2020-06-22 19:24:42 +0530

    Updates

---
+++ precache-manifest.baf1ec18ac81addd3c486ff6fcb10cdd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1aebb38a174693eeb7b1d50057a3d3d",
+    "url": "/Resume-Reactjs/index.html"
+  },
+  {
+    "revision": "3396d1d99739d2c7e9dc",
+    "url": "/Resume-Reactjs/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6d2a0d11df3c06076670",
+    "url": "/Resume-Reactjs/static/js/2.4d931468.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Resume-Reactjs/static/js/2.4d931468.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3396d1d99739d2c7e9dc",
+    "url": "/Resume-Reactjs/static/js/main.b7aeb28f.chunk.js"
+  },
+  {
+    "revision": "729445f880be635fca23",
+    "url": "/Resume-Reactjs/static/js/runtime-main.9367b114.js"
+  },
+  {
+    "revision": "27c8b60137db6b7ea0dbc678fb995b22",
+    "url": "/Resume-Reactjs/static/media/image1.27c8b601.svg"
+  },
+  {
+    "revision": "169046d1ba06b65ac9d6d78ac605cdc8",
+    "url": "/Resume-Reactjs/static/media/image2.169046d1.svg"
+  }
+]);
